f74310f55f48e027080928e08fee0826bc6ed1d1
ynhanjun <hanjunedu@qq.com>
2022-05-26 08:35:45 +0800

    提交修改

--- exercise/jsonp.php
+++ exercise/jsonp.php
@@ -1,5 +1,7 @@
 <?php
-header("Content-type: application/javsscript");
+header("Content-type: application/javascript");
 $data = ['msg'=>'Hello World!'];
 $json = json_encode($data);
 echo "test({$json})";
+
+// test({"msg":"Hello World!"})
